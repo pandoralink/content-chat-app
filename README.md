7cfd1d4106099e96aa49a0813790515c102806e6
poplink <2219535001@qq.com>
2022-09-05 01:15:20 +0800

    feat: 更新 README.md

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
+<p align="center"><img alt="APP 图片" align="center" width="100px" height="100px" src="./static/ic_book_128.svg"/></p><br />
+<p align="center">
+  <a href="https://baike.baidu.com/item/X%2FMIT%E8%AE%B8%E5%8F%AF%E5%8D%8F%E8%AE%AE/10136122?fr=aladdin"><img src="https://img.shields.io/github/license/pandoralink/CommunityInteractionProject-H5" alt="License"></a>
+</p>
+
+此项目为互动社区 `APP` 的 `Android` 原生 `APP`
+
+> 别名（alias）：内容型社交 APP，为我寒假实习/暑期实习/校招（也许还有春招？）H5 方向的项目经历曾经的名称
+
+_前端方向导航_
+
+# 相关项目
+
+1. 项目的 `H5` 页面部分，[CommunityInteractionProject-H5](https://github.com/pandoralink/CommunityInteractionProject-H5)
+2. 基于上面那个原生 `Android`，迁移至 `React-Native + expo` 跨端技术栈，[CommunityInteractionProject-expo](https://github.com/pandoralink/CommunityInteractionProject-expo)
+2. 原生 `Android` 开发 `APP`，技术栈为 `java`，[content-chat-app](https://github.com/pandoralink/content-chat-app)
+
 # 摘要
 
 为了用户在阅读平台提供文章内容的同时，具有即时分享，互动交互，用户间交互等社交型内容，该项目在 Android 系统上采用相关技术开发了此 APP
--- app/src/main/java/com/example/newslist/data/Constants.java
+++ app/src/main/java/com/example/newslist/data/Constants.java
@@ -7,13 +7,13 @@ public final class Constants {
     private Constants() {
     }
 
-    public static String SERVER_URL = "http://116.63.152.202:5002/";
-    public static String TEST_NEW_URL = "http://116.63.152.202:5002/News/demo.html";
-    public static final String BACK_BASE_URL = "http://116.63.152.202:3002/";
+    public static String SERVER_URL = "http://114.132.66.80:5002/";
+    public static String TEST_NEW_URL = "http://114.132.66.80:5002/News/demo.html";
+    public static final String BACK_BASE_URL = "http://114.132.66.80:3002/";
     public static final String ARTICLE_URL = BACK_BASE_URL + "new";
     public static final String FOLLOW_AUTHOR_ARTICLE_URL = BACK_BASE_URL + "fan";
     public static final String ARTICLE_AUTHOR_INFO_BASE_URL = BACK_BASE_URL + "authorInfo";
-    public static final String ARTICLE_AUTHOR_INFO_UPDATE_BASE_URL = "http://116.63.152.202:8084/updateRole";
+    public static final String ARTICLE_AUTHOR_INFO_UPDATE_BASE_URL = "http://114.132.66.80:8084/updateRole";
     public static final String USER_ARTICLE_URL = BACK_BASE_URL + "userArticle";
     public static String ARTICLE_URL_KEY = "article_url_key";
     public static String ARTICLE_AUTHOR_INFO_KEY = "article_author_info_key";
@@ -24,8 +24,8 @@ public final class Constants {
     public static String USER_RELATE_KEY = "user_relate_key";
     public static String AUTHOR_FAN_TOTAL_KEY = "author_fan_total_key";
     public static String LOCAL_WEBSOCKET_URL = "ws://10.0.2.2:8181";
-    public static String REMOTE_WEBSOCKET_URL = "ws://116.63.152.202:8181";
+    public static String REMOTE_WEBSOCKET_URL = "ws://114.132.66.80:8181";
     public static final String LOCAL_LOGIN_BASE_URL = "http://localhost:8084/login";
-    public static final String REMOTE_LOGIN_BASE_URL = "http://116.63.152.202:8084/login";
-    public static final String REMOTE_REGISTER_BASE_URL = "http://116.63.152.202:8084/register";
+    public static final String REMOTE_LOGIN_BASE_URL = "http://114.132.66.80:8084/login";
+    public static final String REMOTE_REGISTER_BASE_URL = "http://114.132.66.80:8084/register";
 }
